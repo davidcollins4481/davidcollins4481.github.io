337bf1696bd8bc520a7719d71dfe282c7cd8af31
David Collins <davidcollins4481@gmail.com>
2014-11-27 23:43:33 -0500

    more layout fixes

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,7 @@ var cssbasedir = themebasedir + 'stylesheets/';
 var paths = {
     scripts: [],
     stylesheets: [
+        cssbasedir + 'src/font-awesome.css',
         cssbasedir + 'src/styles.scss'
     ]
 };
